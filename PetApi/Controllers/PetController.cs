3f6bc6303c8595a3127b643b34fc825937f9c422
Bingqi <BLiu25@slb.com>
2022-10-31 14:17:35 +0800

    feat: ac1

---
+++ PetApi/Controllers/PetController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetApiTest.Controller;
+
+namespace PetApi.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class PetController
+    {
+        [HttpPost("addNewPet")]
+        public Pet AddNewPet(Pet pet)
+        {
+            return pet;
+        }
+    }
+}
---
+++ PetApiTest/ControllerTest/PetControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using PetApiTest.Controller;
+using Xunit;
+
+namespace PetApiTest.ControllerTest
+{
+    public class PetController
+    {
+        [Fact]
+        public async void Should_add_new_pet_to_system_successfully()
+        {
+            // given
+            var application = new WebApplicationFactory<Program>();
+            var httpclient = application.CreateClient();
+            /*
+             *Method:post
+             *URI: /api/addNewPet
+             *Body:
+             * {
+             *"name":"kitty",
+             *"type":"cat",
+             *"color":"white",
+             *"price":1000
+             * }
+             */
+            var pet = new Pet(name: "Kitty", type: "cat", color: "white", price: 1000);
+            var serializeObject = JsonConvert.SerializeObject(pet);
+            var postBody = new StringContent(serializeObject, Encoding.UTF8, "application/json");
+            var response = await httpclient.PostAsync("/api/addNewPet", postBody);
+            //then
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var savedPet = JsonConvert.DeserializeObject<Pet>(responseBody);
+            Assert.Equal(pet, savedPet);
+        }
+    }
+}
